ca2d145c338e62b75b58d3707607cd9007b774bf
krishgupta1 <krishgupta11110@gmail.com>
2023-06-02 21:37:49 +0530

    Last Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
-
+import { useContext, useEffect } from "react";
+import { AppContext } from "./context/AppContext";
+import Header from "./components/Header";
+import Blogs from "./components/Blog";
+import Pagination from "./components/Pagination";
 export default function App() {
-  
+  const { fetchBlogPosts } = useContext(AppContext);
+
+  useEffect(() => {
+    // Fetch the inital Blogposts data
+    fetchBlogPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>
-      <Header/>
-      <Blog/>
-      <Pagination/>
-    </div>
+    <>
+      <Header />
+      <div className="my-[100px]">
+        <Blogs />
+        <Pagination />
+      </div>
+    </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='w-full border shadow-md py-4 top-0 fixed bg-white'>
+      <header className='text-center uppercase'>
+        <h1 className='text-3xl font-bold'>CodeHelp Blogs</h1>
+      </header>
     </div>
   )
 }
